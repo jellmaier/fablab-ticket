d24675b31c4ea08f511dabebc7dfba1abf3e03b1
Jakob Ellmaier <jakob.ellmiaer@student.tugraz.at>
2015-09-17 16:38:40 +0200

    update version number

--- fablab-ticket.php
+++ fablab-ticket.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: FabLab Ticket
-Version: 0.2.5
+Version: 0.2.6
 Plugin URI: https://github.com/jellmaier/fablab-ticket
 Description: Ticketing and Reservation System for FabLabs
 Author: Jakob Ellmaier
