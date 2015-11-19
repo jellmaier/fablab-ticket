a9ef164522bb1b24b593dc586f30cfa25602ad00
Jakob Ellmaier <jakob.ellmiaer@student.tugraz.at>
2015-11-19 13:47:42 +0100

    initial release for productive mode in fablab + design adaptation for new theme

--- fablab-ticket.php
+++ fablab-ticket.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: FabLab Ticket
-Version: 0.4.12
+Version: 0.5.0
 Plugin URI: https://github.com/jellmaier/fablab-ticket
 Description: Ticketing and Reservation System for FabLabs
 Author: Jakob Ellmaier
