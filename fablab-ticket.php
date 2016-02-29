32aab3f48f9803970461f70ca3780709e05dfdda
Jakob Ellmaier <jakob.ellmiaer@student.tugraz.at>
2016-02-29 01:15:10 +0100

    bigger buttons in admin ticket list

--- fablab-ticket.php
+++ fablab-ticket.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: FabLab Ticket
-Version: 0.6.9
+Version: 0.6.10
 Plugin URI: https://github.com/jellmaier/fablab-ticket
 Description: Ticketing and Reservation System for FabLabs
 Author: Jakob Ellmaier
