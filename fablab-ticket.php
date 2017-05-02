a7e6543b3243a8f49a9b0d9526a33f72e185540c
Jakob Ellmaier <ellmiaer@tugraz.at>
2017-05-02 16:08:37 +0200

    + change auto_logout color on final seconds

--- fablab-ticket.php
+++ fablab-ticket.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: FabLab Ticket
-Version: 1.9.3
+Version: 1.9.4
 Plugin URI: https://github.com/jellmaier/fablab-ticket
 Description: Ticketing and Reservation System for FabLabs
 Author: Jakob Ellmaier
