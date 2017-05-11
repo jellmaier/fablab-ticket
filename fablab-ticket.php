71784ff4182fdb4ffa75f217e5cb26741ae1bbf7
Jakob Ellmaier <ellmiaer@tugraz.at>
2017-05-11 17:35:37 +0200

    + small nfc-login fixes

--- fablab-ticket.php
+++ fablab-ticket.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: FabLab Ticket
-Version: 1.9.5
+Version: 1.9.6
 Plugin URI: https://github.com/jellmaier/fablab-ticket
 Description: Ticketing and Reservation System for FabLabs
 Author: Jakob Ellmaier
